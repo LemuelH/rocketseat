80f2453b93003266840cf41e415e056d4c0e39cd
Lemuel Henrique <47530945+LemuelH@users.noreply.github.com>
2022-03-05 23:25:20 -0300

    Add files via upload

--- Modulo JS/Exercicio01_JS.js
+++ Modulo JS/Exercicio01_JS.js
@@ -7,19 +7,28 @@
 // * entre 60 - 69   -   D
 // * menor que 60    -   F
 
-let nota = 85.3;
+function getNota(nota) {
+  let notaA = nota > 90 && nota <= 100;
+  let notaB = nota >= 80 && nota <= 89;
+  let notaC = nota >= 70 && nota <= 79;
+  let notaD = nota >= 80 && nota <= 69;
+  let notaF = nota < 60 && nota == 0;
 
-if (nota > 90) {
-  var notaCarac = "A";
-} else if (nota >= 80 && nota <= 89) {
-  var notaCarac = "B";
-} else if (nota >= 70 && nota <= 79) {
-  var notaCarac = "C";
-} else if (nota >= 80 && nota <= 69) {
-  var notaCarac = "D";
-} else if (nota >= 80 && nota <= 9) {
-} else {
-  var notaCarac = "F";
+  if (notaA) {
+    notaCarac = "A";
+  } else if (notaB) {
+    notaCarac = "B";
+  } else if (notaC) {
+    notaCarac = "C";
+  } else if (notaD) {
+    notaCarac = "D";
+  } else if (notaF) {
+    notaCarac = "F";
+  } else{
+    console.log("NOTA INVÁLIDA")
+    return
+  }
+  console.log(`${nota} é ${notaCarac}`);
 }
 
-console.log(`${nota} é ${notaCarac}`);
+getNota(75);
